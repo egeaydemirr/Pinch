41b4aa43e0e49de608ecfb0373aeed533f05a802
Ege Aydemir <egeaydemirr97@gmail.com>
2022-09-28 01:34:44 +0300

    Double Tab Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     //MARK: - Property
-    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
     //MARK: - Function
     
     //MARK: - Content
@@ -24,11 +25,28 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x:2, y: 2)
-                
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                //MARK: -1. TAB GESTURE
+                    .onTapGesture(count: 2, perform:  {
+                        if imageScale == 1 {
+                            withAnimation(.spring()){
+                                imageScale = 5
+                            }
+                        }
+                        else {
+                            imageScale = 1
+                        }
+                    })
                 
             }// ZStack:
             .navigationTitle("Pinch and Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating = true
+                }
+            })
         }// NavigationView
         .navigationViewStyle(.stack )
     }
