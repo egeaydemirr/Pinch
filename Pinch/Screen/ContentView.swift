1c47bf47dc59f962ca56e26e5f1d59a0d040a08b
Ege Aydemir <egeaydemirr97@gmail.com>
2022-09-27 21:16:21 +0300

    Photo Edit

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,19 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - Property
+    
+    //MARK: - Function
+    
+    //MARK: - Content
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack{
+                // MARK: PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x:2, y: 2)
+                
+                
+            }// ZStack:
+            .navigationTitle("Pinch and Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+        }// NavigationView
+        .navigationViewStyle(.stack )
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
