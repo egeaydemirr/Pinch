313164fc3a134c87d10ae9cda280c9611a045f03
Ege Aydemir <egeaydemirr97@gmail.com>
2022-09-27 17:29:36 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ege Aydemir on 27.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
