5f9725b71c17db5029f89b45a15163f6c89c1c34
Ege Aydemir <egeaydemirr97@gmail.com>
2022-09-29 23:34:29 +0300

    final touches of long gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -26,6 +26,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack{
+                Color.clear
                 // MARK: PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -71,6 +72,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            //MARK: Info Panel
+            .overlay(
+            InfoPanelView(scale: imageScale, offset: imageOffset)
+                .padding(.horizontal)
+                .padding(.top, 30)
+            , alignment: .top
+            )
         }// NavigationView
         .navigationViewStyle(.stack )
     }
