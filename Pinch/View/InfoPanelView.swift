bbd7d5e86baf45d717533884e33e58eaf0534ebe
Ege Aydemir <egeaydemirr97@gmail.com>
2022-09-29 23:24:20 +0300

    Long Press Gesture

--- Pinch/View/InfoPanelView.swift
+++ Pinch/View/InfoPanelView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct InfoPanelView: View {
     var scale : CGFloat
     var offset : CGSize
+    @State private var isInfoPanelVisible : Bool = false
+    
     var body: some View {
         HStack{
             //MARK: HOTSPOT
@@ -17,6 +19,11 @@ struct InfoPanelView: View {
                 .symbolRenderingMode(.hierarchical)
                 .resizable()
                 .frame(width: 30, height: 30)
+                .onLongPressGesture(minimumDuration: 1) {
+                    withAnimation(.easeOut){
+                        isInfoPanelVisible.toggle()
+                    }
+                }
             Spacer()
             //MARK: INFO PANEL
             HStack(spacing: 2){
@@ -40,6 +47,7 @@ struct InfoPanelView: View {
             .background(.ultraThinMaterial)
             .cornerRadius(8)
             .frame(maxWidth: 420)
+            .opacity(isInfoPanelVisible ? 1 : 0)
            
             Spacer()
             
