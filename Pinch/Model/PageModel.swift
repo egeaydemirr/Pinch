414646522c4f9a9beb6b42073b77b5f3de3fa843
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-10 16:04:34 +0300

    Data Model

---
+++ Pinch/Model/PageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PageModel.swift
+//  Pinch
+//
+//  Created by Ege Aydemir on 10.10.2022.
+//
+
+import Foundation
+
+struct Page: Identifiable {
+    let id : Int
+    let imageName : String  
+}
