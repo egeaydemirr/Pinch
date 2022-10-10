7ec8e39863a04a0f742eeaddceb99c1bc9886bc9
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-10 16:12:37 +0300

    Data File

---
+++ Pinch/Data/PagesData.swift
@@ -0,0 +1,16 @@
+//
+//  PagesData.swift
+//  Pinch
+//
+//  Created by Ege Aydemir on 10.10.2022.
+//
+
+import Foundation
+
+let pagesData : [Page] = [
+Page( id: 1, imageName: "magazine-front-cover"),
+Page( id: 2, imageName: "magazine-back-cover")
+]
+
+
+
